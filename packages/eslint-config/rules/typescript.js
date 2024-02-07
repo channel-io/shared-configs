d03cf8d8a35ee98a7fa22e1c99aa680f6cd2a3c3
John <ddasfasd123@gmail.com>
2024-02-07 12:05:23 +0900

    fix: set base rules

--- packages/eslint-config/rules/typescript.js
+++ packages/eslint-config/rules/typescript.js
@@ -1,10 +1,50 @@
 module.exports = {
-  '@typescript-eslint/no-unused-vars': ['error'],
-  '@typescript-eslint/consistent-type-imports': ['warn'],
-  'no-dupe-class-members': 'off', // Allow method overriding (will be checked by tsc)
-  'no-undef': 'off',
-  'no-unused-vars': 'off',
-  '@channel.io/pascal-case-enum-name': ['error', true, false],
-  '@channel.io/pascal-case-interface-name': ['error'],
-  '@channel.io/pascal-case-type-name': ['error'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      extends: ['plugin:import/typescript'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        // eslint-plugin-react@7.32.2 accesses superTypeParameters, which is deprecated by
+        // typescript-estree and prints a warning by default
+        suppressDeprecatedPropertyWarnings: true,
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/consistent-type-imports': ['warn'],
+        '@typescript-eslint/consistent-type-assertions': [
+          'warn',
+          { assertionStyle: 'as', objectLiteralTypeAssertions: 'allow' },
+        ],
+        '@typescript-eslint/no-extra-non-null-assertion': 'warn',
+
+        // Overrides
+        'no-dupe-class-members': 'off',
+        '@typescript-eslint/no-dupe-class-members': 'warn',
+
+        'no-redeclare': 'off',
+        '@typescript-eslint/no-redeclare': 'warn',
+
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            args: 'none',
+            ignoreRestSiblings: true,
+            caughtErrors: 'all',
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+        'no-undef': 'off',
+      },
+      settings: {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+        },
+      },
+    },
+  ],
 }
